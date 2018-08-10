4e932bdaced9688babe0d0cdf3f9775e64fa2c76
EC2 Default User <ec2-user@ip-172-31-38-233.us-east-2.compute.internal>
2018-08-10 00:59:40 +0000

    gem shusei

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_22_160808) do
+ActiveRecord::Schema.define(version: 2018_08_10_004416) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2018_07_22_160808) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "status"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
